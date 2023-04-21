3d852211f20e4391983a876b84bd9b3dafc76e1c
보니 <73548875+Guboneui@users.noreply.github.com>
2023-04-21 09:39:15 +0900

    템플릿 변경

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -6,116 +6,122 @@ import ProjectDescription
 /// See https://docs.tuist.io/guides/helpers/
 
 extension Project {
+  
+  
+  /// Helper function to create a framework target and an associated unit test target
+  private static func makeFrameworkTarget(
+    name: String,
+    platform: Platform,
+    iOSTargetVersion: String,
+    isDynamic: Bool,
+    dependencies: [TargetDependency]
+  ) -> [Target] {
+    let sources = Target(name: name,
+                         platform: platform,
+                         product: isDynamic ? .framework : .staticFramework,
+                         bundleId: "come.deco.ios.\(name)",
+                         deploymentTarget: .iOS(targetVersion: "15.0", devices: .iphone),
+                         infoPlist: .default,
+                         sources: ["Sources/**"],
+                         resources: ["Resources/**"],
+                         dependencies: [])
+    
+    return [sources]
+  }
+  
+  /// Helper function to create the application target and the unit test target.
+  private static func makeAppTargets(
+    name: String,
+    platform: Platform,
+    iOSTargetVersion: String,
+    infoPlist: [String: InfoPlist.Value],
+    dependencies: [TargetDependency]
+  ) -> [Target] {
+    let platform: Platform = platform
+    let infoPlist: [String: InfoPlist.Value] = [
+      "CFBundleShortVersionString": "1.0",
+      "CFBundleVersion": "1",
+      "UIMainStoryboardFile": "",
+      "UILaunchStoryboardName": "LaunchScreen"
+    ]
+    
+    let additionalSetting: [String:SettingValue] = [:]
+    
+    let baseSetting: [String:SettingValue] = [
+      "DEVELOPMENT_TEAM": "VKGAQDGK5R",
+    ]
+    
+    let mainTarget = Target(
+      name: name,
+      platform: platform,
+      product: .app,
+      bundleId: "come.deco.ios.\(name)",
+      infoPlist: .extendingDefault(with: infoPlist),
+      sources: ["Sources/**"],
+      resources: ["Resources/**"],
+      dependencies: dependencies,
+      settings: .settings(
+        base: baseSetting.merging(additionalSetting, uniquingKeysWith: { first, _ in first }),
+        configurations: [],
+        defaultSettings: .recommended
+      )
+    )
     
     
-    /// Helper function to create a framework target and an associated unit test target
-    private static func makeFrameworkTarget(
-        name: String,
-        platform: Platform,
-        iOSTargetVersion: String,
-        dependencies: [TargetDependency]
-    ) -> [Target] {
-        let sources = Target(name: name,
-                             platform: platform,
-                             product: .framework,
-                             bundleId: "come.deco.ios.\(name)",
-                             deploymentTarget: .iOS(targetVersion: "15.0", devices: .iphone),
-                             infoPlist: .default,
-                             sources: ["Sources/**"],
-                             resources: ["Resources/**"],
-                             dependencies: [])
-        
-        return [sources]
-    }
+    return [mainTarget]
+  }
+  
+  private static let organizationName = "Boni"
+  
+  public static func app(name: String,
+                         platform: Platform,
+                         iOSTargetVersion: String,
+                         infoPlist: [String: InfoPlist.Value],
+                         dependencies: [TargetDependency] = []) -> Project {
+    let targets = makeAppTargets(name: name,
+                                 platform: platform,
+                                 iOSTargetVersion: iOSTargetVersion,
+                                 infoPlist: infoPlist,
+                                 dependencies: dependencies)
+    return Project(name: name,
+                   organizationName: organizationName,
+                   targets: targets)
+  }
+  
+  public static func frameworkWithTestApp(name: String,
+                                          platform: Platform,
+                                          iOSTargetVersion: String,
+                                          isDynamic: Bool,
+                                          infoPlist: [String: InfoPlist.Value] = [:],
+                                          dependencies: [TargetDependency] = []) -> Project {
+    var targets = makeFrameworkTarget(name: name,
+                                      platform: platform,
+                                      iOSTargetVersion: iOSTargetVersion,
+                                      isDynamic: isDynamic,
+                                      dependencies: dependencies)
+    targets.append(contentsOf: makeAppTargets(name: "\(name)TestApp",
+                                              platform: platform,
+                                              iOSTargetVersion: iOSTargetVersion,
+                                              infoPlist: infoPlist,
+                                              dependencies: [.target(name: name)]))
     
-    /// Helper function to create the application target and the unit test target.
-    private static func makeAppTargets(
-        name: String,
-        platform: Platform,
-        iOSTargetVersion: String,
-        infoPlist: [String: InfoPlist.Value],
-        dependencies: [TargetDependency]
-    ) -> [Target] {
-        let platform: Platform = platform
-        let infoPlist: [String: InfoPlist.Value] = [
-            "CFBundleShortVersionString": "1.0",
-            "CFBundleVersion": "1",
-            "UIMainStoryboardFile": "",
-            "UILaunchStoryboardName": "LaunchScreen"
-        ]
-        
-        let additionalSetting: [String:SettingValue] = [:]
-        
-        let baseSetting: [String:SettingValue] = [
-            "DEVELOPMENT_TEAM": "VKGAQDGK5R",
-        ]
-        
-        let mainTarget = Target(
-            name: name,
-            platform: platform,
-            product: .app,
-            bundleId: "come.deco.ios.\(name)",
-            infoPlist: .extendingDefault(with: infoPlist),
-            sources: ["Sources/**"],
-            resources: ["Resources/**"],
-            dependencies: dependencies,
-            settings: .settings(
-                base: baseSetting.merging(additionalSetting, uniquingKeysWith: { first, _ in first }),
-                configurations: [],
-                defaultSettings: .recommended
-            )
-        )
-        
-        
-        return [mainTarget]
-    }
-    
-    private static let organizationName = "Boni"
-    
-    public static func app(name: String,
-                           platform: Platform,
-                           iOSTargetVersion: String,
-                           infoPlist: [String: InfoPlist.Value],
-                           dependencies: [TargetDependency] = []) -> Project {
-        let targets = makeAppTargets(name: name,
-                                     platform: platform,
-                                     iOSTargetVersion: iOSTargetVersion,
-                                     infoPlist: infoPlist,
-                                     dependencies: dependencies)
-        return Project(name: name,
-                       organizationName: organizationName,
-                       targets: targets)
-    }
-    
-    public static func frameworkWithTestApp(name: String,
-                                            platform: Platform,
-                                            iOSTargetVersion: String,
-                                            infoPlist: [String: InfoPlist.Value] = [:],
-                                            dependencies: [TargetDependency] = []) -> Project {
-        var targets = makeFrameworkTarget(name: name,
-                                           platform: platform,
-                                           iOSTargetVersion: iOSTargetVersion,
-                                           dependencies: dependencies)
-        targets.append(contentsOf: makeAppTargets(name: "\(name)TestApp",
-                                                  platform: platform,
-                                                  iOSTargetVersion: iOSTargetVersion,
-                                                  infoPlist: infoPlist,
-                                                  dependencies: [.target(name: name)]))
-        
-        return Project(name: name,
-                       organizationName: organizationName,
-                       targets: targets)
-    }
-    
-    public static func framework(name: String,
-                                 platform: Platform, iOSTargetVersion: String,
-                                 dependencies: [TargetDependency] = []) -> Project {
-        let targets = makeFrameworkTarget(name: name,
-                                           platform: platform,
-                                           iOSTargetVersion: iOSTargetVersion,
-                                           dependencies: dependencies)
-        return Project(name: name,
-                       organizationName: organizationName,
-                       targets: targets)
-    }
+    return Project(name: name,
+                   organizationName: organizationName,
+                   targets: targets)
+  }
+  
+  public static func framework(name: String,
+                               platform: Platform,
+                               iOSTargetVersion: String,
+                               isDynamic: Bool,
+                               dependencies: [TargetDependency] = []) -> Project {
+    let targets = makeFrameworkTarget(name: name,
+                                      platform: platform,
+                                      iOSTargetVersion: iOSTargetVersion,
+                                      isDynamic: isDynamic,
+                                      dependencies: dependencies)
+    return Project(name: name,
+                   organizationName: organizationName,
+                   targets: targets)
+  }
 }

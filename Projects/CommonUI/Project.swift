99b6538d286d3bea5468932cb638d81a69ad71e2
보니 <73548875+Guboneui@users.noreply.github.com>
2023-04-25 21:18:26 +0900

    CommonUI에 flex, pin, then 의존

--- Projects/CommonUI/Project.swift
+++ Projects/CommonUI/Project.swift
@@ -8,12 +8,54 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 private let projectName: String = "CommonUI"
-private let iOSTargetVersion: String = "15.0"
+//private let iOSTargetVersion: String = "15.0"
+//
+//let project = Project.framework(
+//  name: projectName,
+//  platform: .iOS,
+//  iOSTargetVersion: "15.0",
+//  isDynamic: true,
+//  dependencies: []
+//)
 
-let project = Project.framework(
+let setting: Settings = .settings(
+  base: [
+    "GCC_PREPROCESSOR_DEFINITIONS" : "FLEXLAYOUT_SWIFT_PACKAGE=1",
+    "DEVELOPMENT_TEAM": "VKGAQDGK5R",
+//    "OTHER_LDFLAGS": "-ObjC"
+  ],
+  configurations: [],
+  defaultSettings: .recommended
+)
+
+let packages: [Package] = [
+  .FlexLayout,
+  .PinLayout,
+  .Then
+]
+
+let dependencies: [TargetDependency] = [
+  .FlexLayout,
+  .PinLayout,
+  .Then
+]
+
+let commonUI = Target(
   name: projectName,
   platform: .iOS,
-  iOSTargetVersion: "15.0",
-  isDynamic: true,
-  dependencies: []
+  product: .framework,
+  productName: projectName,
+  bundleId: "com.deco.ios.\(projectName)",
+  deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
+  infoPlist: .default,
+  sources: ["Sources/**"],
+  resources: ["Resources/**"],
+  dependencies: dependencies,
+  settings: setting
+)
+
+let project = Project(
+  name: projectName,
+  packages: packages,
+  targets: [commonUI]
 )

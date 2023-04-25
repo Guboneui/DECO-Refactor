94493a718a841c9879824f11232f2d70ce2bebcb
보니 <73548875+Guboneui@users.noreply.github.com>
2023-04-25 21:17:32 +0900

    Util staticframework로 ribs의존

--- Projects/Util/Project.swift
+++ Projects/Util/Project.swift
@@ -8,59 +8,55 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-private let projectName: String = "Util"
-private let iOSTargetVersion: String = "15.0"
-
-let project = Project.framework(
-  name: projectName,
-  platform: .iOS,
-  iOSTargetVersion: "15.0",
-  isDynamic: true,
-  packages: [
+//private let projectName: String = "Util"
+//private let iOSTargetVersion: String = "15.0"
+//
+//let project = Project.framework(
+//  name: projectName,
+//  platform: .iOS,
+//  iOSTargetVersion: "15.0",
+//  isDynamic: true,
+//  packages: [
 //    .RIBs
+//  ],
+//  dependencies: [
+//    .RIBs
+//  ]
+//)
+
+let setting: Settings = .settings(
+  base: [
+    "DEVELOPMENT_TEAM": "VKGAQDGK5R",
+//    "OTHER_LDFLAGS": "-ObjC"
   ],
-  dependencies: [
-//    .RIBs,
-//    .RxSwift,
-  ]
+  configurations: [],
+  defaultSettings: .recommended
 )
 
-//import ProjectDescription
-//import ProjectDescriptionHelpers
-//
-//let setting: Settings = .settings(
-//  base: [
-//    "DEVELOPMENT_TEAM": "VKGAQDGK5R",
-//    "OTHER_LDFLAGS": "-ObjC"
-//  ],
-//  configurations: [],
-//  defaultSettings: .recommended
-//)
-//
-//let packages: [Package] = [
-//  .RIBs
-//]
-//
-//let dependencies: [TargetDependency] = [
-//  .RIBs
-//]
-//
-//let util = Target(
-//  name: "Util",
-//  platform: .iOS,
-//  product: .staticFramework,
-//  productName: "Util",
-//  bundleId: "com.deco.ios.Util",
-//  deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
-//  infoPlist: .default,
-//  sources: ["Sources/**"],
-//  resources: ["Resources/**"],
-//  dependencies: dependencies,
-//  settings: setting
-//)
-//
-//let project = Project(
-//  name: "util",
-//  packages: packages,
-//  targets: [util]
-//)
+let packages: [Package] = [
+  .RIBs
+]
+
+let dependencies: [TargetDependency] = [
+  .RIBs
+]
+
+let util = Target(
+  name: "Util",
+  platform: .iOS,
+  product: .staticFramework,
+  productName: "Util",
+  bundleId: "com.deco.ios.Util",
+  deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
+  infoPlist: .default,
+  sources: ["Sources/**"],
+  resources: ["Resources/**"],
+  dependencies: dependencies,
+  settings: setting
+)
+
+let project = Project(
+  name: "Util",
+  packages: packages,
+  targets: [util]
+)

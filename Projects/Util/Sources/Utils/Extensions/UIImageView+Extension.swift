b4c28129a7870ac96ac68b629d2628fb195f23a7
보니 <73548875+Guboneui@users.noreply.github.com>
2023-05-16 15:00:12 +0900

    [FEAT] Nuke Image Load

---
+++ Projects/Util/Sources/Utils/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  UIImageView+Extension.swift
+//  Util
+//
+//  Created by 구본의 on 2023/05/16.
+//
+
+import UIKit
+import Nuke
+
+public extension UIImageView {
+  func loadLowQualityImage(imageUrl: String, _ completion: (() -> Void)? = nil) {
+    Nuke.DataLoader.sharedUrlCache.removeAllCachedResponses()
+    (ImagePipeline.shared.configuration.dataLoader as? DataLoader)?.session.configuration.urlCache?.removeAllCachedResponses()
+    let resizeProcessor = ImageProcessors.Resize(size: CGSize(width: 50, height: 50))
+    let request = ImageRequest(url: URL(string: imageUrl),
+                               processors: [resizeProcessor],
+                               priority: .high)
+    Nuke.loadImage(with: request, into: self) { _ in
+      completion?()
+    }
+  }
+  
+  func loadImage(imageUrl: String, _ completion: (() -> Void)? = nil) {
+    Nuke.DataLoader.sharedUrlCache.removeAllCachedResponses()
+    (ImagePipeline.shared.configuration.dataLoader as? DataLoader)?.session.configuration.urlCache?.removeAllCachedResponses()
+    let resizeProcessor = ImageProcessors.Resize(size: CGSize(width: 300, height: 300))
+    let request = ImageRequest(url: URL(string: imageUrl),
+                               processors: [resizeProcessor],
+                               priority: .high)
+    Nuke.loadImage(with: request, into: self) { _ in
+      completion?()
+    }
+  }
+}
+
